8897a06c4802833d59d0dc11f5853e924c081d06
aditya gajghate <adityagajghate45@gmail.com>
2025-09-28 11:49:19 +0530

    west

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
-// let score = "hitesh"
+let score = "hitesh"
 // console.log(typeof score);
 // console.log(typeof(score));
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
@@ -10,8 +10,9 @@
 // "33abc" => NaN (Not a Number)
 // true => 1; false > 0
 
-// let isLoggedIn = "hitesh"
-// let booleanIsLoggedIn =Boolean(isLoggedIn)
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 //  console.log(booleanIsLoggedIn);
 
 
@@ -21,5 +22,42 @@
 
 let someNumber = 33
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//******************* Operations ********************/
+
+let value =3
+let negValue = -value
+// console.log(negValue);  //-3
+
+
+//operations in JS
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2)
+// console.log(2+3);
+// console.log(2/3)
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"     //subtration not possible in string
+// let str3 = str1 - str2
+// console.log(str3);  //NaN
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);         // "1" + 2 => "1" + "2" => "12"
+// console.log(1+"2");         // 1 + "2" => "1" + "2" => "12"
+// console.log("1"+2+2);       // "1" + 2 + 2 => "1" + "2" + 2 => "12" + 2 => "122"
+// console.log(1+2+"2");       //left to right // 1 + 2 + "2" => 3 + "2" => "32"
+// console.log("hello"+2+2);   // "hello" + 2 + 2 => "hello2" + 2 => "hello22"
+
+//first is string then all will be string
+//first is number then all will be number
+// console.log((3+4)*5%3) 
+
+console.log(true);//true
+console.log(+true);//1
+console.log(-true);//-1
+
